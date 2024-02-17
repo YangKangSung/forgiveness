d51c0659f61cf74fbb60e3359f6cac71607c509a
YangKangSung <cyclestel@gmail.com>
2024-02-17 13:05:19 +0900

    get magnetlink

--- main.py
+++ main.py
@@ -2,31 +2,14 @@ import requests
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 
+base_url="https://torrenttop88.com"
 def extract_links(keyword):
-  """
-  토렌트 검색 결과에서 링크 추출
-
-  Args:
-    keyword: 검색어
-
-  Returns:
-    링크 목록
-  """
-
-  # URL 생성
-  url = f"https://torrenttop88.com/search/index?keywords={keyword}"
-
-  # HTML 응답 가져오기
+  url = f"{base_url}/search/index?keywords={keyword}"
   response = requests.get(url)
   response.raise_for_status()
-
-  # BeautifulSoup 객체 생성
   soup = BeautifulSoup(response.content, "html.parser")
 
-  # 토렌트 항목 목록 추출
   items = soup.find_all("div", class_="py-4 flex flex-row border-b topic-item")
-
-  # 각 항목에서 href 속성 추출
   links = []
   for item in items:
     title = item.find("a")["title"]
@@ -35,16 +18,26 @@ def extract_links(keyword):
         links.append({'title': title, 'href': href})
   return links
 
-# 검색어 입력
-keyword = input("검색어를 입력하세요: ")
+def extract_magnet_links(href):
+	url = f"{base_url}{href}"
+	print(f"{url=}")
 
-# URL을 인코딩
-encoded_keyword = quote(keyword)
+	response = requests.get(url)
+	response.raise_for_status()
 
-# 링크 추출
+	soup = BeautifulSoup(response.content, "html.parser")
+	items = soup.find_all("div", class_="p-3")
+	print(items)
+
+encoded_keyword = quote(input("검색어를 입력하세요: "))
 links = extract_links(encoded_keyword)
-
-# 결과 출력
 print(f"총 {len(links)}개의 링크를 찾았습니다.")
 for link in links:
   print(link)
+
+first_item = links[0] if links else None
+print(f"{first_item=}")
+
+link = extract_magnet_links(first_item['href'])
+
+
